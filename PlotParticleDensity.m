531e5996caf95b46b397a0fc322075f3efb25ddc
jonathanlevine <jonathanaaron.levine@gmail.com>
2022-02-03 23:12:24 -0500

    Updated particle density and temeprature plotting functions

--- PlotParticleDensity.m
+++ PlotParticleDensity.m
@@ -1,14 +1,13 @@
 function PlotParticleDensity(states)
     global world;
     values = hist3([states(:,1), states(:,2)], [200 100], 'CdataMode','auto');
-    imagesc(values.')
+    image = imboxfilt(values, 11);
+    imagesc(image.')
+    xlim([0 world.length/(10^(-9))]);
+    ylim([0 world.height/(10^(-9))]);
     c = colorbar;
     c.Label.String = 'Number of Particles';
     xlabel('x (nm)');
     ylabel('y (nm)');
-    xlim([0 world.length/(10^(-9))]);
-    ylim([0 world.height/(10^(-9))]);
-    axis equal
-    axis xy
 end
 
--- PlotTemperatureMap.m
+++ PlotTemperatureMap.m
@@ -1,11 +1,12 @@
 function PlotTemperatureMap(states)
     x = states(:,1);
     y = states(:,2);
-    xx = linspace(min(x), max(x), 200);
+    xx = linspace(min(x), max(x), 400);
     yy = linspace(min(y), max(y), 200);
     [X, Y] = meshgrid(xx, yy);
     Z = griddata(x, y, states(:,5), X, Y);
-    surf(X, Y, Z);
+    image = imboxfilt(Z, 5);
+    imagesc(image)
     c = colorbar;
     c.Label.String = 'Particle Temperature (K)';
     xlabel('x (nm)');
