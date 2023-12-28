6ae8101523a416f9e434c90eef054444b67b28c7
jonathanalevine <jonathanaaron.levine@gmail.com>
2023-12-28 18:02:17 -0500

    barebones run file done

--- run.m
+++ run.m
@@ -12,14 +12,47 @@ k = 1.38064852*10^(-23); %Boltzmann Constant
 tau = 0.2*10^(-12);
 
 global world;
-world.length = 200*10^(-9);
-world.height = 100*10^(-9);
+world.length = 400*10^(-9);
+world.height = 200*10^(-9);
 
 global vth;
 vth = sqrt(2*k*T/m);
 
+global dt;
 dt = world.height/vth/100;
+
+epochs = 1000;
+
 Pscat = 1 - exp(-dt/tau);
 
 % The mean free path = velocity*mean time between collisions
 MFP = vth * tau;
+
+% Simulation controls
+num_particles = 100;
+distribution_type = 'MB';
+epochs = 1000;
+scattering = 1;
+
+% generate the initial states of all the particles
+states = GenerateStates(num_particles, distribution_type);
+
+for epoch = 1:epochs
+    figure(1)
+    % plot the particles in the world
+    PlotAllParticles(states);
+    states = WorldBoundaryHandler(states, 1);
+    % handle scattering
+    if scattering
+        states = ScatterParticle(states);
+    end
+    % move the particles
+    states = MoveParticle(states);
+    % print the epoch in terminal
+    epoch
+    pause(0.01);
+end
+
+
+
+
